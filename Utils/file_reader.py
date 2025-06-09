af333970c5a839fc3ae67646400201c6e03e59da
Victor <victorsouzalima2064@gmail.com>
2025-06-09 16:10:11 -0300

    labels retropropagation implemented

--- Utils/file_reader.py
+++ Utils/file_reader.py
@@ -4,15 +4,22 @@ from datetime import datetime
 def get_files(dir, output_file, backspace, date):
 
     authorized = False
+    verification_flag = True
 
-    if not os.path.isfile(dir) and bool(os.listdir(dir)):
+    # check if dir is a file or an empty folder
+    if not os.path.isfile(dir) and bool(os.listdir(dir)):  
         files = os.listdir(dir)
 
         for file in files:
             newDir = os.path.join(dir, file).replace("\\", "/")
-            
 
-            authorized = get_files(newDir, output_file, backspace=backspace+"|________", date= date)
+            return_value = get_files(newDir, output_file, backspace=backspace+"|________", date= date)
+
+            if not return_value:
+                verification_flag = False
+
+        if verification_flag:
+            authorized = True
     else:
         mod_time = os.path.getmtime(dir)
         mod_date = datetime.fromtimestamp(mod_time).date()
@@ -22,5 +29,6 @@ def get_files(dir, output_file, backspace, date):
 
     output_file.write(backspace + dir + " " + str(authorized) + '\n')
     print(authorized)
+
     return authorized
         
