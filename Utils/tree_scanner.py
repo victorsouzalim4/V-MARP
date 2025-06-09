25482d18be7eb12273ad946cfe96f0de92fbeb22
Victor <victorsouzalima2064@gmail.com>
2025-06-09 17:11:51 -0300

    function renamed for better legibility

--- Utils/tree_scanner.py
+++ Utils/tree_scanner.py
@@ -1,18 +1,17 @@
 import os
 from datetime import datetime
 
-def get_files(current_path, output_file, indent, limit_date):
+def map_authorized_files(current_path, output_file, indent, limit_date):
     is_authorized = False
     all_children_authorized = True
 
-    # check if current_path is a file or an empty folder
-    if not os.path.isfile(current_path) and bool(os.listdir(current_path)):  
+    if not os.path.isfile(current_path) and bool(os.listdir(current_path)):  # check if current_path is a file or an empty folder
         entries = os.listdir(current_path)
 
         for entry in entries:
             child_path = os.path.join(current_path, entry).replace("\\", "/")
 
-            child_authorization = get_files(
+            child_authorization = map_authorized_files(
                 child_path, output_file, 
                 indent = indent + "|________", 
                 limit_date = limit_date
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from Utils.file_reader import get_files
+from Utils.tree_scanner import map_authorized_files
 from datetime import datetime
 
 dir = 'C:/Users/Valle/OneDrive/Documentos/Neural_Networks/Neural_Networks'
@@ -7,4 +7,8 @@ output_file = 'output.txt'
 date = datetime.strptime("2025-06-09", "%Y-%m-%d").date()
 
 with open(output_file, "w", encoding="utf-8") as f:
-    get_files(dir=dir, output_file=f, backspace="", date=date)
+    map_authorized_files(
+        current_path = dir, 
+        output_file=f, indent = "",
+        limit_date = date
+    )
