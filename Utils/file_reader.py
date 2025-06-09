4b6416f0436b8df9848ba653bc273f708f4902e0
Victor <victorsouzalima2064@gmail.com>
2025-06-09 14:57:40 -0300

    get_time of the ladt modification implemented

--- Utils/file_reader.py
+++ Utils/file_reader.py
@@ -1,6 +1,8 @@
 import os
+from datetime import datetime
 
 def get_files(dir, output_file, backspace):
+
 
     if not os.path.isfile(dir):
         files = os.listdir(dir)
@@ -11,4 +13,8 @@ def get_files(dir, output_file, backspace):
             output_file.write(backspace + newDir + '\n')
 
             get_files(newDir, output_file, backspace=backspace+"|________")
+    else:
+        mod_time = os.path.getmtime(dir)
+        mod_date = datetime.fromtimestamp(mod_time).strftime('%Y-%m-%d %H:%M:%S')
+        print(mod_date)
         
